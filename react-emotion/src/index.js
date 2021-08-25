d1d1c94421d0743b458eb5873da929a51bec65b4
Felix Roos <roos@entrecode.de>
2021-08-25 11:23:32 +0200

    add react-shadow

--- react-emotion/src/index.js
+++ react-emotion/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import GlobalStyles from './styles/GlobalStyles'
 import App from './App'
+import root from 'react-shadow';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <root.div> {/* this breaks styles */}
+      <GlobalStyles />
+      <App />
+    </root.div>
   </React.StrictMode>,
   document.getElementById('root'),
 )
